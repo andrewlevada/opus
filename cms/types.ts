20a64a5da521faa0cd2a543fa1f5683d0c5cca40
AndrewLevada <levada.andrew@gmail.com>
2022-07-15 11:32:04 +0300

    :tada: basic file structure and data types

---
+++ cms/types.ts
@@ -0,0 +1,28 @@
+export interface Activity {
+	id: string;
+	title: Localized<string>;
+	short?: Localized<string>;
+	description: Localized<string>;
+	tags: Tag[];
+
+	// TODO: Make dates validatable
+	started: string;
+	ended: string;
+
+	isTestList?: boolean;
+
+	links?: {
+		label: Localized<string>;
+		url: string;
+	}[];
+}
+
+export type Tag = SkillTag | EnvTag | StackTag;
+type SkillTag = "code" | "ux" | "product" | "design";
+type EnvTag = "web" | "game" | "irl" | "chat-bot";
+type StackTag = "typescript" | "javascript" | "csh" | "unity" | "figma" | "adobe" | "java";
+
+export type Localized<T> = {
+	en: T;
+	ru: T;
+}
